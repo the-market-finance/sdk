238efe38ded90d2956f16210c6c8f0befef23bc4
Ширнин Алексей <shirnin@Prom.local>
2021-01-17 13:34:45 +0300

    filter lending token by pk

--- src/actions/common.ts
+++ src/actions/common.ts
@@ -1,18 +1,21 @@
 import {LENDING_PROGRAM_ID} from "../constants";
 import {calculateBorrowAPY, isLendingReserve, LendingReserve, LendingReserveParser} from "../models/lending";
-import {Connection} from "@solana/web3.js";
+import {Connection, PublicKey} from "@solana/web3.js";
 import {ParsedAccount} from "../contexts/accounts";
 
 
-export const getReserveAccounts = async (connection: Connection) => {
+export const getReserveAccounts = async (connection: Connection, address?: string | PublicKey) => {
+    const id = typeof address === "string" ? address : address?.toBase58();
     const programAccounts = await connection.getProgramAccounts(
         LENDING_PROGRAM_ID
     );
-    return programAccounts
+    const lendingReserveAccounts = programAccounts
         .filter(item =>
             isLendingReserve(item.account))
         .map((acc) =>
             LendingReserveParser(acc.pubkey, acc.account)).filter( acc => acc !== undefined
         ) as ParsedAccount<LendingReserve>[]
+
+    return !id ? lendingReserveAccounts : lendingReserveAccounts.filter(acc => acc?.pubkey.toBase58() === id)
 }
 
